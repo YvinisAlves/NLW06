2381b1f2e39f39366c8060caca4efb29e0f83bb1
YvinisAlves <55796360+YvinisAlves@users.noreply.github.com>
2021-06-22 23:17:01 -0300

    DIa 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import {AuthContextProvider } from './contexts/AuthContext'
+import { Room } from "./pages/Room";
 
 function App() {
   
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact={true} component={Home}/>
-        <Route path="/rooms/new" component={NewRoom}/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/rooms/new" component={NewRoom}/>
+          <Route path="/rooms/:id" component={Room}/>
+        </Switch>
       </AuthContextProvider>
    </BrowserRouter>
   );
 }
-
 export default App;
